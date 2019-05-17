437ade66e7dbdcfd6e879064b12dea5616ab83f8
valentinavega <valentinavega94@gmail.com>
2019-05-17 18:37:30 -0400

    Add event listener to form

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 function onReady() {
    const toDos = [];
+   const addToDoForm = document.getElementById('addToDoForm');
 
    function createNewToDo() {
      const newToDoText = document.getElementById('newToDoText');
+     toDos.push({
+       title: newToDoText.value,
+       complete: false
+});
+      newToDoText.value = '';
    }
 }
+
+addToDoForm.addEventListener('submit', event => {
+  event.preventDefault();
+  createNewToDo();
+});
 
 window.onload = function() {
   onReady();
