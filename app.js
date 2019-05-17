57afb935cb44ecd7a78565ed900db84d1aa22d78
valentinavega <valentinavega94@gmail.com>
2019-05-16 23:02:41 -0400

    change 2

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
 
   addToDoForm.addEventListener('submit', () => {
     event.preventDefault();
