2724aaf4605cf4b896ea7e42a21a72a0630e10a4
valentinavega <valentinavega94@gmail.com>
2019-05-17 18:46:12 -0400

    Update

--- app.js
+++ app.js
@@ -4,17 +4,36 @@ function onReady() {
 
    function createNewToDo() {
      const newToDoText = document.getElementById('newToDoText');
+     if (!newToDoText.value) { return; }
+
      toDos.push({
        title: newToDoText.value,
        complete: false
 });
       newToDoText.value = '';
+      renderTheUI();
    }
 }
 
-addToDoForm.addEventListener('submit', event => {
-  event.preventDefault();
-  createNewToDo();
+function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+
+    addToDoForm.addEventListener('submit', event => {
+      event.preventDefault();
+      createNewToDo();
 });
 
   renderTheUI();
