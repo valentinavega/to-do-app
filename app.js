c688b33675c6e9fb3f9f76a9536aafca8c2bdacc
valentinavega <valentinavega94@gmail.com>
2019-05-18 17:02:20 -0400

    Update

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 function onReady() {
-   const toDos = [];
-   const addToDoForm = document.getElementById('addToDoForm');
+  let toDos = [];
+  let id = 0;
+  const addToDoForm = document.getElementById('addToDoForm');
 
-   function createNewToDo() {
-     const newToDoText = document.getElementById('newToDoText');
-     if (!newToDoText.value) { return; }
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) {
+      return;
+    }
 
-     toDos.push({
-       title: newToDoText.value,
-       complete: false
-});
-      newToDoText.value = '';
-      renderTheUI();
-   }
-}
+    toDos.push({
+      title: newToDoText.value,
+      complete: false,
+      id: id
+    });
 
-function renderTheUI() {
+    id++;
+    newToDoText.value = '';
+    renderTheUI();
+  }
+
+  function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
-
     toDoList.textContent = '';
 
     toDos.forEach(function(toDo) {
@@ -25,16 +29,32 @@ function renderTheUI() {
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
 
+
       newLi.textContent = toDo.title;
 
-      toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
-    });
+      toDoList.appendChild(newLi);
 
-    addToDoForm.addEventListener('submit', event => {
-      event.preventDefault();
-      createNewToDo();
-});
+      let button = document.createElement('button');
+      button.type = "submit";
+      button.textContent = "Delete!";
+
+      button.onclick = function(event) {
+        toDos = toDos.filter(function(existingList) {
+          return existingList.id !== toDo.id;
+        });
+        renderTheUI();
+      };
+
+      newLi.appendChild(button);
+
+    });
+  }
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+  });
 
   renderTheUI();
 }
